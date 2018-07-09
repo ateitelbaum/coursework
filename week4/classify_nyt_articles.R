cdf218662af6c0c3b70976c173fea60dccac6366
ayliana <ateitelbaum2000@gmail.com>
2018-07-09 10:31:13 -0400

    week4

--- week4/classify_nyt_articles.R
+++ week4/classify_nyt_articles.R
@@ -3,6 +3,8 @@ library(tm)
 library(Matrix)
 library(glmnet)
 library(ROCR)
+library(caret)
+library(broom)
 
 ########################################
 # LOAD AND PARSE ARTICLES
@@ -10,8 +12,8 @@ library(ROCR)
 
 # read in the business and world articles from files
 # combine them both into one data frame called articles
-business <- read_tsv('business.tsv')
-world <- read_tsv('world.tsv')
+business <- read_tsv('nyt_business.tsv',quote = "\'")
+world <- read_tsv('nyt_world.tsv', quote = "\'")
 articles <- rbind(business, world)
 
 # create a corpus from the article snippets
@@ -36,25 +38,43 @@ set.seed(42)
 ########################################
 
 # create a train / test split
+train_index <- sample(1:nrow(X), 0.8*nrow(X))
+trainX <- X[train_index, ]
+trainY <- articles$section_name[train_index]
+test_index <- setdiff(seq(1,2000), train_index)
+testX <- X[test_index, ]
+testY <- data.frame("section" = articles$section_name[test_index])
 
 # cross-validate logistic regression with cv.glmnet (family="binomial"), measuring auc
+glm <- cv.glmnet(trainX, trainY, family = "binomial", type.measure = "auc")
 
 # plot the cross-validation curve
+plot(glm)
 
 # evaluate performance for the best-fit model
 # note: it's useful to explicitly cast glmnet's predictions
 # use as.numeric for probabilities and as.character for labels for this
+testY$pred <- as.numeric(predict(glm, testX, type = "response"))
+testY$label <- as.factor(predict(glm, testX, type = "class"))
 
 # compute accuracy
+testY %>% summarize(acc = mean(label == section))
 
 # look at the confusion matrix
+confusionMatrix(data = as.factor(testY$section), reference = as.factor(testY$label))
 
 # plot an ROC curve and calculate the AUC
 # (see last week's notebook for this)
+pred <- prediction(testY$pred, testY$section)
+perf <- performance(pred, measure = 'tpr', x.measure = 'fpr')
+plot(perf)
+performance(pred, 'auc')
 
 # show weights on words with top 10 weights for business
 # use the coef() function to get the coefficients
 # and tidy() to convert them into a tidy data frame
+wordCoef <- tidy(coef(glm)) 
+wordCoef %>% filter(value < 0) %>% arrange(value) %>% head(10) %>% View()
 
 # show weights on words with top 10 weights for world
-
+wordCoef %>% arrange(desc(value)) %>% head(10) %>% View()
--- week4/nyt_api_section.py
+++ week4/nyt_api_section.py
@@ -19,6 +19,8 @@ if __name__=='__main__':
    pages = number_articles // 10
    extra = number_articles % 10
 
+   print("section_name", "url", "pub_date", "snippet", sep='\t', file = codecs.open('nyt_'+section_name+'.tsv', 'a+', encoding = 'utf-8'))
+
    for i in range(0, pages):
       params = {'api-key': api_key, 'fq': "section_name:"+section_name, 'sort': "newest", 'page': i}
       r = requests.get(ARTICLE_SEARCH_URL, params)
@@ -26,8 +28,8 @@ if __name__=='__main__':
       time.sleep(1)
 
       for doc in data['response']['docs']:
-         snippet = doc['snippet'].replace('\n', '')
-         print(section_name, doc['web_url'], doc['pub_date'], snippet, sep='\t', file = open('nyt_'+section_name+'.tsv', 'a+'))
+         snippet = doc['snippet'].replace('&#xa', '')
+         print(section_name, doc['web_url'], doc['pub_date'], snippet, sep='\t', file = codecs.open('nyt_'+section_name+'.tsv', 'a+', encoding = 'utf-8'))
 
    params = {'api-key': api_key, 'fq': "section_name:"+section_name, 'sort': "newest", 'page': pages}
    r = requests.get(ARTICLE_SEARCH_URL, params)
@@ -36,8 +38,8 @@ if __name__=='__main__':
 
    for j in range(0, extra):
       doc = data['response']['docs'][j]
-      snippet = doc['snippet'].replace('\n', '')
-      print(section_name, doc['web_url'], doc['pub_date'], snippet, sep='\t', file = open('nyt_'+section_name+'.tsv', 'a+'))
+      snippet = doc['snippet'].replace('&#xa', '')
+      print(section_name, doc['web_url'], doc['pub_date'], snippet, sep='\t', file = codecs.open('nyt_'+section_name+'.tsv', 'a+', encoding = 'utf-8'))
 
 
    
